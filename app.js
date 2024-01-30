163b9dcd3d6567d2f5d1a87476209e0cacb59750
RuanV-S <ruan_vieiradasilva@hotmail.com>
2024-01-29 22:27:42 -0300

    update

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require("cors")
 const bookRoutes = require('./routes/book')
-const port = 8000
 app.use(express.json())
-app.use('/book', bookRoutes)
+app.use(cors({ origin: "*"}))
 
+app.use('/books', bookRoutes)
+const port = 8000
 app.listen(port, () => {
   console.log(`Escutando a porta ${port}`)
 })
